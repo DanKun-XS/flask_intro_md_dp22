7fd21aa36479b8827aa68048691bd6c41d37fcc1
[Students] Daniils Kunavins <92DPDKunavins@rvt.lv>
2020-10-23 16:15:54 +0300

    getParam

--- main.py
+++ main.py
@@ -18,4 +18,25 @@ def contact():
 def chat():
     return render_template('chat.html')
 
+@app.route('/getParam')
+def getParam():
+    #print(request.args.get('vards'))
+    return f"It Works! {request.args.get('vards')}"
+
+@app.route('/postMsg', methods = ['POST'])
+def postMsg():
+    #print(request.json)
+    data = request.json
+    return f"This is post! {data['vatds']}"
+
+@app.route('/info', methods = ['GET', 'POST'])
+def info():
+    if request.method == "GET":
+        return "This is info GET route!"
+    elif request.method == "POST":
+        return "This is info POST route!"
+    else:
+        return "Unknow request method!"
+
+
 app.run(host='0.0.0.0', port=80, debug=True)
